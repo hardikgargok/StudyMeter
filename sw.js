c6af3c7b746315aecbd0fe95ca8b7077810e82cc
hardikgargok <hardikgargok@gmail.com>
2026-06-24 23:38:56 +0530

    Polish StudyMeter dashboard and command center

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'studymeter-v3';
+const CACHE_NAME = 'studymeter-v4';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './studymeter-icon.svg'];
 
 self.addEventListener('install', event => {
